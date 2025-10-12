2bfde4ea5ee6467851e250892f93c368e016aee2
Haller Bracho <habracho@fec.luz.edu.ve>
2025-10-12 14:23:07 -0400

    Add files via upload

--- clasesluz.py
+++ clasesluz.py
@@ -1244,7 +1244,7 @@ with tab_examen:
                             
                             if quiz_data:
                                 time.sleep(2)
-                                random.shuffle(quiz_data)
+                                if not config.get('show_feedback', 1): random.shuffle(quiz_data)
                                 
                                 num_a_presentar = config['num_preguntas']
                                 quiz_subset = quiz_data[:num_a_presentar]
